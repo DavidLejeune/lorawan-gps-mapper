85c2916d10f425b02f989e1f6a630071f2829167
Niels Van Vlaenderen <nielsvanvlaenderen@hotmail.com>
2016-11-07 11:35:19 +0100

    db:seed en setup, toevoegen van aantal voorbeeldgebruikers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+case Rails.env
+when "development"
+   User.create(name: 'admin', email:'admin@admin.be')
+   User.create(name: 'david', email:'david@admin.be')
+   User.create(name: 'niels', email:'niels@admin.be')
+   User.create(name: 'milan', email:'milan@admin.be')
+   User.create(name: 'nicolas', email:'nicolas@admin.be')
+when "production"
+   User.create(name: 'admin', email:'admin@admin.be')
+end
