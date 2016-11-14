c55d85790e56f21ca538a4eb5c68869edc657c9e
David Lejeune <Lejeune.David@outlook.com>
2016-11-14 11:30:14 +0100

    Validating the presence of a name attribute

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,14 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+
 end
