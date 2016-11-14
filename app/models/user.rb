d1a55dc8a89b12c027b522a1453ed4478479f8b6
David Lejeune <Lejeune.David@outlook.com>
2016-11-14 22:00:39 +0100

    Disallowing double dots in email domain names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                   format: { with: VALID_EMAIL_REGEX }
 
   validates :password, presence: true, length: { maximum: 50 }
 
   validates :password_confirmation, presence: true, length: { maximum: 50 }
-  
+
 end
