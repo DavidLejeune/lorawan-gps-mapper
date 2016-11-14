b1f496a317f9eb4fff6d7ae0a994e2e01d2e3010
David Lejeune <Lejeune.David@outlook.com>
2016-11-14 22:02:08 +0100

    A test for the rejection of duplicate email addresses.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,4 +14,12 @@ class UserTest < ActiveSupport::TestCase
   end
 
 
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+
 end
