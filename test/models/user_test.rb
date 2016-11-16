d85c0f8fb6d362db7a84d89d17a6b32f0e4cd7fb
Sille Van Landschoot <info@sillevl.be>
2016-11-16 10:14:10 +0100

    merged user tests from app/model directory to test file

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,14 +6,17 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(
+						name: "Example User",
+						email: "user@example.com",
+						password: "mysecurepassword", 
+						password_confirmation: "mysecurepassword"
+					)
   end
 
   test "should be valid" do
     assert @user.valid?
   end
-
-
 
   test "email addresses should be unique" do
     duplicate_user = @user.dup
@@ -21,5 +24,24 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+	test "name should be present" do
+		@user.name = "     "
+		assert_not @user.valid?
+	end
+
+	test "email should be present" do
+		@user.email = "     "
+		assert_not @user.valid?
+	end
+
+	test "password should be present" do
+		@user.password = ""
+		assert_not @user.valid?
+	end
+		  
+	test "password_confirmation should be present" do
+		@user.password_confirmation = ""
+		assert_not @user.valid?
+	end
 
 end
