fe50e56fd02f3a01034a288a55fd276fee59e30e
David Lejeune <Lejeune.David@outlook.com>
2016-11-20 16:57:05 +0100

    Listing 8.7: Finding and authenticating a user.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+    else
+      # Create an error message.
+      render 'new'
+    end
   end
 
   def destroy
