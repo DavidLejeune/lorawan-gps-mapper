56fb22a0db243359a88b195943d0eff23f5e0151
Milan Lamote <milanlamote@hotmail.com>
2016-11-28 10:04:32 +0100

    job reads long and lat and outputs into shell

---
+++ app/jobs/mqtt_job.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'mqtt'
+require 'json'
+
+class MqttJob < ApplicationJob
+  queue_as :default
+
+ttn_host = "staging.thethingsnetwork.org"
+port = 1883
+username = '70B3D57ED000165A'
+password = 'FbHtfC4+cAXmgkAicItU15hBbrdQdKcu/tBrwE+UjVw='
+sensor_id = '+/devices/+/up'
+
+# Publish example
+  MQTT::Client.connect(
+  :host => ttn_host,
+  :port => port,
+  :username => username,
+  :password => password) do |c|
+    # If you pass a block to the get method, then it will loop
+    c.get(sensor_id) do |topic,message|
+      obj = JSON.parse("#{message}")
+      latitude = obj['fields']['lat']
+      longitude = obj['fields']['long']
+      puts "latitude = #{latitude}"
+      puts "longitude = #{longitude}"
+
+    end
+  end
+end
