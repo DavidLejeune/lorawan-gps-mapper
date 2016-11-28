34a15449fdfd173fef3595990adf647ada45fbe5
Niels Van Vlaenderen <nielsvanvlaenderen@hotmail.com>
2016-11-28 11:08:10 +0100

    generate scaffold Location

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :locations
   resources :data
   resources :sensors
   root 'static_pages#home'
---
+++ db/migrate/20161128094406_create_locations.rb
@@ -0,0 +1,12 @@
+class CreateLocations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :locations do |t|
+      t.float :longitude
+      t.float :latitude
+      t.integer :height
+      t.integer :accuracy
+
+      t.timestamps
+    end
+  end
+end
